0bed09f8b7a5f7d4276d8062054bc2db02815f23
ziang.chen@student.unimelb.edu.au <774726628@qq.com>
2018-10-04 15:12:53 +1000

    A

---
+++ assignment3/src/mycontroller/MyAIController.java
@@ -0,0 +1,20 @@
+package mycontroller;
+
+import controller.CarController;
+import world.Car;
+
+public class MyAIController extends CarController {
+
+	public MyAIController(Car car) {
+		super(car);
+	}
+
+	@Override
+	public void update() {
+		// TODO Auto-generated method stub
+		
+	}
+	
+	
+
+}
