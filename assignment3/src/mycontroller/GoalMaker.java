917839ab029da2e7a774402e9dc7ae75d6cff194
zack0518 <774726628@qq.com>
2018-10-17 20:09:00 +1100

    update mycontroller

--- assignment3/src/mycontroller/GoalMaker.java
+++ assignment3/src/mycontroller/GoalMaker.java
@@ -1,51 +1,142 @@
 package mycontroller;
-
+import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.Random;
-
+import tiles.LavaTrap;
 import tiles.MapTile;
+import tiles.TrapTile;
 import utilities.Coordinate;
+import world.Car;
 
 public class GoalMaker {
+	
 	public int stateReward;
-	Coordinate targetPoint;
+	public Coordinate targetPoint;
 	public int mapWidth;
 	public int mapHeight;
-	HashMap<Coordinate, MapTile> currentMap;
+	public HashMap<Coordinate, MapTile> currentMap;
+	public ArrayList <Coordinate> futureGoal;
+	public HashMap<Coordinate, MapTile> currentView;
+	public ArrayList <Coordinate> visitedGoals;
+	public StuckedWarning stuckWarn;
+	public Car car;
 	
-	public GoalMaker (int mapWidth, int mapHeight, HashMap<Coordinate, MapTile> currentMap){
+	GoalMaker (int mapWidth, int mapHeight, HashMap<Coordinate, MapTile> currentMap, Car car){
 		this.mapWidth = mapWidth;
 		this.mapHeight = mapHeight;
 		this.currentMap = currentMap;
+		futureGoal = new ArrayList <Coordinate>();
+		visitedGoals = new ArrayList <Coordinate>();
+		stuckWarn = new StuckedWarning(car);
+		this.car = car;
+		predefinedGoals();
 	}
 	
-	public Coordinate getConers() {
+	public void predefinedGoals() {
+		Coordinate currPos = new Coordinate(this.car.getPosition());
 		Coordinate corner1 = new Coordinate(0,0);
-		Coordinate corner2 = new Coordinate(mapWidth - 1, 0);
-		Coordinate corner3 = new Coordinate(mapWidth - 1, mapHeight - 1);
-		Coordinate corner4 = new Coordinate(0, mapHeight - 1);
+		if(!isValidGoal(corner1)) {
+			corner1 = getClosestValidCoordinate(corner1);
+		}
+		Coordinate corner2 = new Coordinate(mapWidth - 10, 0);
+		if(!isValidGoal(corner2)) {
+			corner2 = getClosestValidCoordinate(corner2);
+		}
+		Coordinate corner3 = new Coordinate(mapWidth - 10, mapHeight - 10);
+		if(!isValidGoal(corner3)) {
+			corner3 = getClosestValidCoordinate(corner3);
+		}
+		Coordinate corner4 = new Coordinate(0, mapHeight - 10);
+		if(!isValidGoal(corner4)) {
+			corner4 = getClosestValidCoordinate(corner4);
+		}
+		Coordinate midPoint = new Coordinate(mapWidth/2, mapHeight/2);
+		if(!isValidGoal(midPoint)) {
+			midPoint = getClosestValidCoordinate(midPoint);
+		}
+		ArrayList <Coordinate>  candidateGoal = new ArrayList <Coordinate> ();
 		
+		candidateGoal.add(corner1);
+		candidateGoal.add(corner2);
+		candidateGoal.add(midPoint);
+		candidateGoal.add(corner3);
+		candidateGoal.add(corner4);	
 		
-		return null;
-	}
-	
-	
-	public void evaluateCurrView(HashMap<Coordinate, MapTile> currentView, Coordinate myPos) {
-		System.out.println("---------------------");
-		Coordinate targetPoint = null;
-		int currManhattanDistance = 0;
-		int maxManhattanDistance = 0;
-		for (Coordinate c : currentView.keySet()) {
-			currManhattanDistance = Math.abs(myPos.x - c.x) + Math.abs(myPos.y - c.y);
-			if (currManhattanDistance > maxManhattanDistance) {
-				 maxManhattanDistance = currManhattanDistance;
-				 targetPoint = c;
+		for (Coordinate c : candidateGoal) {
+			if (getManhattanDistance(c, currPos) > 5) {
+				futureGoal.add(c);
 			}
 		}
-		this.targetPoint =  targetPoint;
-		System.out.println("---------------------");
 	}
 	
+	public boolean isValidGoal(Coordinate coordinate) {
+		if (currentMap.get(coordinate).getType() == MapTile.Type.ROAD) {
+			return true;
+		}
+		return false;
+	}
+	
+	public void reachedTheGoal(Coordinate c) {
+		futureGoal.remove(c);
+		visitedGoals.add(c);
+	}
+	public Coordinate getClosestValidCoordinate(Coordinate coordinate) {
+		int minDistance = Integer.MAX_VALUE;
+		Coordinate target = coordinate;
+		for (Coordinate c : currentMap.keySet()) {
+			if (getManhattanDistance(c, coordinate) < minDistance && this.currentMap.get(c).isType(MapTile.Type.ROAD)) {
+				minDistance = getManhattanDistance(c, coordinate);
+				target = c;
+			}
+		}
+		return target;
+	}
+	
+	public int getManhattanDistance(Coordinate c1, Coordinate c2) {
+		return Math.abs(c1.x - c2.x) + Math.abs(c1.y - c2.y);
+	}
+	
+	public void evaluateCurrentView(HashMap<Coordinate, MapTile> currentView) {
+		for(Coordinate c : currentView.keySet()) {
+			boolean isVisitedGoal = isVisitedGoal(c);
+			if(currentView.get(c).getType() == MapTile.Type.TRAP && !isVisitedGoal) {
+				shouldTrapBecomeGoal((TrapTile) currentView.get(c), c);
+				
+				TrapTile trapTile = (TrapTile) currentView.get(c);
+				if (shouldTrapBecomeGoal(trapTile, c)) {
+					visitedGoals.add(c);
+				}
+			}
+		}
+	}
+	
+	public boolean isVisitedGoal(Coordinate currGoal) {
+		for(Coordinate c : visitedGoals) {
+			if (c.x == currGoal.x && c.y == currGoal.y) {
+				return true;
+			}
+		}
+		return false;
+	}
+	
+	public boolean shouldTrapBecomeGoal(TrapTile trapTile, Coordinate c) {
+		if(trapTile.getTrap().equals("lava")){
+			LavaTrap lavaTrap = (LavaTrap) trapTile;
+			if (lavaTrap.getKey() > 0) {
+				System.out.println("key found : "+lavaTrap.getKey()) ;
+				System.out.println("key on : "+c);
+				return true;
+			}
+			
+		}else if(trapTile.getTrap().equals("health")) {
+			return true;
+		}
+		return false;
+	}
+	/**
+	 * get the random goal for testing purpose
+	 * @return
+	 */
 	public Coordinate getRandomGoal() {
 		Random rand = new Random();
 		int n = rand.nextInt(currentMap.size());
@@ -60,7 +151,7 @@ public class GoalMaker {
 	}
 	
 	public Coordinate getCurrGoal() {
-		return this.targetPoint;
+
+		return futureGoal.get(0);
 	}
-	
 }
--- assignment3/src/mycontroller/MyAIController.java
+++ assignment3/src/mycontroller/MyAIController.java
@@ -1,64 +1,154 @@
 package mycontroller;
+
 import java.util.HashMap;
+import java.util.HashSet;
 import java.util.Iterator;
-import java.util.Map;
+import java.util.List;
+
+import com.badlogic.gdx.graphics.g3d.model.data.ModelMaterial.MaterialType;
+import com.badlogic.gdx.graphics.g3d.particles.ParticleSorter.Distance;
+import com.badlogic.gdx.math.Path;
+import com.badlogic.gdx.utils.Queue;
+
 import controller.CarController;
 import tiles.MapTile;
+import tiles.MapTile.Type;
+import tiles.MudTrap;
 import utilities.Coordinate;
 import world.Car;
 import world.WorldSpatial;
+import world.WorldSpatial.Direction;
+import mycontroller.*;
 
 public class MyAIController extends CarController {
 	
-		// How many minimum units the wall is away from the player.
-		private int wallSensitivity = 4;
-		private boolean isFollowingWall = false; // This is set to true when the car starts sticking to a wall.			
-		// Car Speed to move at
-		private final int CAR_MAX_SPEED = 1;
-		private DetectAroundSensor detectAroundSensor;
-		private GoalMaker currGoal;
-		private Coordinate currDistination;
-		private MoveSimulation moveSimulation;
-		private int MapWidth;
-		private int MapHeight;
-		
-		public MyAIController(Car car) {
-			super(car);
-			detectAroundSensor = new DetectAroundSensor(this.wallSensitivity, car);
-			moveSimulation = new MoveSimulation();
-			currGoal = new GoalMaker(mapWidth(), mapHeight(), getMap());
-			currDistination = currGoal.getRandomGoal();
-			System.out.println(currDistination);
-		}
-		// Coordinate initialGuess;
-		// boolean notSouth = true;
-		@Override
-		public void update() {
-			//Gets what the car can see
-			HashMap<Coordinate, MapTile> currentView = getView();
-			for (Coordinate c : currentView.keySet()) {
-				System.out.println(c);
-			}
-			HashMap<Coordinate, MapTile> currentMap = getMap();
-			for (Coordinate c : currentMap.keySet()) {
-				if (currentMap.get(c).getType() == MapTile.Type.ROAD) {
-					System.out.println(c);
-					System.out.println(MapTile.Type.ROAD);
-				}
-				
-			}
+	// How many minimum units the wall is away from the player.
+	private int wallSensitivity = 1;
+	
+	private boolean isFollowingWall = false; // This is set to true when the car starts sticking to a wall.
+	
+	// Car Speed to move at
+	private final int CAR_MAX_SPEED = 1;
+	
+	private HashSet<Coordinate> lava  = new HashSet<Coordinate>();
+	private HashSet<Coordinate> grass = new HashSet<Coordinate>();
+	private HashSet<Coordinate> mud = new HashSet<Coordinate>();
+	private HashSet<Coordinate> health = new HashSet<Coordinate>();
+	private HashSet<Coordinate> choose = new HashSet<Coordinate>();
+	private Queue<Coordinate> previous = new Queue<Coordinate>();
+	public HashMap<Coordinate, MapTile> exploreMap = new HashMap<>();
+	private HashSet<Coordinate> closedSet = new HashSet<>();
+	
+	private GoalMaker currGoal;
+	private Coordinate currDistination;
+	private DetectAroundSensor sensor;
+	
+	public MyAIController(Car car) {
+		super(car);
+		sensor = new DetectAroundSensor(wallSensitivity,car);
+		currGoal = new GoalMaker(mapWidth(), mapHeight(), getMap(),car);
+//		currDistination = currGoal.getRandomGoal();
+		Coordinate coordinate = new Coordinate(-1,-1);
+//		System.out.println(currDistination);
+		previous.addFirst(coordinate);
+	}
+	
+	// Coordinate initialGuess;
+	// boolean notSouth = true;
+	@Override
+	public void update() {
+		// Gets what the car can see
+		HashMap<Coordinate, MapTile> currentView = getView();
+		HashMap<Coordinate, MapTile> currentMap = getMap();
 
-			Coordinate currPos = new Coordinate(getPosition());
-			currGoal.evaluateCurrView(currentView, currPos);
-			Coordinate currgoal = currGoal.getRandomGoal();
-			if(getSpeed() < CAR_MAX_SPEED){    // Need speed to turn and progress toward the exit
-				applyForwardAcceleration();  // Tough luck if there's a wall in the way
-			}
-			if (detectAroundSensor.checkWallAheadDistance(getOrientation(), currentView) != wallSensitivity) {
-				WorldSpatial.Direction currDirection;
-				currDirection = moveSimulation.turnLeft(getOrientation(), new Coordinate(getPosition()));
-				currDirection = moveSimulation.turnRight(getOrientation(), new Coordinate(getPosition()));
-			}
-			System.out.println(detectAroundSensor.checkWallAheadDistance(getOrientation(), currentView));
+		Coordinate currentPosition = new Coordinate(getPosition());
+		currGoal.evaluateCurrentView(currentView);
+		currDistination = currGoal.getCurrGoal();
+		System.out.println("dest "+currDistination);
+		Coordinate currPos = new Coordinate(getPosition());
+		if (currPos == currDistination) {
+			currGoal.reachedTheGoal(currPos);
+			applyBrake();
 		}
+		
+		
+		// checkStateChange();
+		if(getSpeed() < CAR_MAX_SPEED){       // Need speed to turn and progress toward the exit
+			applyForwardAcceleration();   // Tough luck if there's a wall in the way
+		}
+		move(currentPosition, currDistination, currentMap);
+	}
+
+
+	private void move(Coordinate currentPosition, Coordinate currDistination, HashMap<Coordinate, MapTile> currentMap) {
+		// TODO Auto-generated method stub
+		List<Coordinate> path = PathFinding.aStarFindPath(currentPosition, currDistination, currentMap);
+		System.out.println(path);
+	    if (path == null) {
+	        throw new IllegalArgumentException("No path to the given destination.");
+	      }
+	      if (path.size() >= 2) {
+	        Coordinate nextPoisition = path.get(1);
+	        moveToGoal(currentPosition, nextPoisition, getOrientation());
+	      }
+		}
+
+	  private void moveToGoal(Coordinate currentPosition, Coordinate nextPoisition, Direction direction) {
+		  Direction relativeDirection = faceToGoal(currentPosition, nextPoisition);
+
+		  if (relativeDirection != direction) {
+			  int directionNum = getNumberOfDirection(direction) - getNumberOfDirection(relativeDirection);
+		      if (directionNum == 1 || directionNum == -3) {
+		        turnLeft();
+		      } else if (directionNum == 2 || directionNum == -2) {
+		    	  if (sensor.checkWallAheadDistance(direction,getView()) >= wallSensitivity) {
+		          turnRight();
+		        } else {
+		          turnLeft();
+		        }
+		      } 
+		      else {
+		        turnRight();
+		      }
+		    }
+		  }
+
+
+	private int getNumberOfDirection(Direction direction) {
+		
+	    if (direction == Direction.EAST) {
+		      return 0;
+		    } else if (direction == Direction.SOUTH) {
+		      return 1;
+		    } else if (direction == Direction.WEST) {
+		      return 2;
+		    } else {
+		      return 3;
+		    }
+	}
+	
+	private Direction faceToGoal(Coordinate currentPosition, Coordinate nextPoisition) {
+	// TODO Auto-generated method stub
+		int deltaX = nextPoisition.x - currentPosition.x;
+		int deltaY = nextPoisition.y - currentPosition.y;
+		
+		if (deltaX > 0) {
+			return Direction.EAST;
+		} else if (deltaX < 0) {
+			return Direction.WEST;
+		} else if (deltaY > 0) {
+			return Direction.NORTH;
+		} else if (deltaY < 0) {
+			return Direction.SOUTH;
+		}
+		
+		return null;
+	}
+	
+	public static int getManhattanDistance(Coordinate a, Coordinate b){
+		int distance = Math.abs(a.x - b.x) + Math.abs(a.y - b.y);
+		return distance;
+	}
+
+	
 }
